2bf87c9b18106ec2abbf8afba1a29d9ec75dd427
Stytchexamples <isaacejeh@stytch.com>
2024-01-11 07:52:13 +0100

    added two new routes for secret rotation

--- payment-server/Helpers/stytch.js
+++ payment-server/Helpers/stytch.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const stytch = require('stytch');
-const mongodbHelpers = require('./mongodb');
 const dotenv = require('dotenv');
 const router = express.Router();
+const NodeCache = require('node-cache');
+const myCache = new NodeCache({
+    stdTTL: 3600,
+    checkperiod: 3600,
+    useClones: false,
+    forceString: true
+});
 
 dotenv.config();
 // Initializing Stytch client
@@ -10,6 +16,30 @@ const client = new stytch.Client({
     project_id: process.env.STYTCH_PROJECT_ID,
     secret: process.env.STYTCH_SECRET
 });
+
+// Get M2M access token
+async function getM2MAccessToken(clientId, clientSecret) {
+    try {
+        // Get M2M access token from node-cache
+        const accessTokenInfo = myCache.get('accessToken');
+        if (accessTokenInfo) return accessTokenInfo;
+        // If the cached token is expired or not available, request a new one
+        const params = {
+            client_id: clientId,
+            client_secret: clientSecret,
+            scopes: ['read:users'], // Adjust scopes as needed
+            grant_type: 'client_credentials'
+        };
+        const response = await client.m2m.token(params);
+        // Save new access token to node-cache
+        myCache.set('accessToken', response.access_token, response.expires_in);
+
+        return response.access_token;
+    } catch (err) {
+        console.error('Error getting M2M access token:', err.response);
+        throw err;
+    }
+}
 
 // Search for an M2M client
 router.get('/search-m2m-client', async (req, res) => {
@@ -64,89 +94,41 @@ router.put('/update-m2m-client/:clientId', async (req, res) => {
     }
 });
 
-
-// Create new M2M client 
-async function getM2MClient(db) {
-    try {
-        // Check if M2M credentials are available in MongoDB
-        const storedCredentials = await mongodbHelpers.getCredentials(db);
-        return storedCredentials;
-    } catch (err) {
-        console.error('Error getting M2M client:', err.response);
-        throw err;
-    }
-}
-
-// Get M2M access token
-async function getM2MAccessToken(db, clientId, clientSecret) {
-    try {
-        // Get M2M access token (cached if possible)
-        const accessTokenInfo = await mongodbHelpers.getAccessToken(db);
-        if (accessTokenInfo && Date.now() < accessTokenInfo.expires_at) {
-            // Use the cached token if it is valid
-            return accessTokenInfo.access_token;
-        } 
-        // If the cached token is expired or not available, request a new one
-        const params = {
-            client_id: clientId,
-            client_secret: clientSecret,
-            scopes: ['read:users'], // Adjust scopes as needed
-            grant_type: 'client_credentials'
-        };
-        const response = await client.m2m.token(params);
-        // Save new access token to db
-        const expiresAt = (Date.now() + response.expires_in) * 1000; // use the provided time milliseconds
-        await mongodbHelpers.storeAccessToken(db, response.access_token, expiresAt);
-      
-        return response.access_token;
-    } catch (err) {
-        console.error('Error getting M2M access token:', err.response);
-        throw err;
-    }
-}
 // Start secret rotation
-async function startSecretRotation(db) {
+router.get('/start-secret-rotation', async (req, res) => {
     try {
-        const client_id = await getM2MClient(db);
+        const clientId = process.env.CLIENT_ID;
         // Start the secret rotation
         const params = {
-            client_id: client_id
+            client_id: clientId
         };
         const response = await client.m2m.clients.secrets.rotateStart(params);
         // Switch the old client_secret for the next_client_secret
-        const m2mClient = {
-            client_id: response.m2m_client.client_id,
-            client_secret: response.m2m_client.next_client_secret
-        };
-        // Store the new credentials securely in MongoDB
-        await mongodbHelpers.storeCredentials(db, m2mClient);
-        await completeSecretRotation(client_id);
-        return m2mClient;
+
+        res.json (response.m2m_client.next_client_secret);
     } catch (err) {
         console.error('Error starting secret rotation:', err.response);
         throw err;
     }
-}
-  
+});
 // Complete the secret rotation
-async function completeSecretRotation(client_id) {
+router.get('/complete-secret-rotation', async (req, res) => {
     try {
+        const clientId = process.env.CLIENT_ID;
         // Permanently switch the client_secret for the next_client_secret
         const params = {
-            client_id: client_id
+            client_id: clientId
         };
         
         await client.m2m.clients.secrets.rotate(params);
+        res.json({ message: 'Secret rotation completed successfully' });
     } catch (err) {
         console.error('Error completing secret rotation:', err.response);
         throw err;
     }
-}
+});
 
 module.exports = {
     router,
-    getM2MClient,
-    getM2MAccessToken,
-    startSecretRotation,
-    completeSecretRotation
+    getM2MAccessToken
 };
